595532b7886d68e56caed925c0469725e29c4444
JENIL <jenil7447@gmail.com>
2026-05-16 18:16:44 +0530

    Refactor routing logic and socket connection handling in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,61 +1,57 @@
-import { createBrowserRouter,RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Navigate } from "react-router-dom";
 import Signup from "./components/Signup";
 import HomePage from "./components/HomePage";
 import Login from "./components/Login";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import io from "socket.io-client";
 import { setSocket } from "./redux/socketSlice";
 import { setOnlineUsers } from "./redux/userSlice";
 
-const router = createBrowserRouter([
-  {
-    path:"/",
-    element:<HomePage/>
-  },
-  {
-    path:"/signup",
-    element:<Signup/>
-  },
-  {
-    path:"/login",
-    element:<Login/>
-  },
-])
-
 function App() {
-  const {authUser} = useSelector(store => store.user);
-  const {socket} = useSelector(store => store.socket);
+  const { authUser } = useSelector(store => store.user);
+  const { socket } = useSelector(store => store.socket);
   const dispatch = useDispatch();
 
-  useEffect(()=>{
-    if(authUser && authUser._id){
-      const socket = io('http://localhost:8080',{
-         query:{
-            userId:authUser._id
-         },
-         withCredentials: true,
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: authUser ? <HomePage /> : <Navigate to="/login" />
+    },
+    {
+      path: "/signup",
+      element: authUser ? <Navigate to="/" /> : <Signup />
+    },
+    {
+      path: "/login",
+      element: authUser ? <Navigate to="/" /> : <Login />
+    },
+  ]);
+
+  useEffect(() => {
+    if (authUser && authUser._id) {
+      const socket = io('http://localhost:8080', {
+        query: { userId: authUser._id },
+        withCredentials: true,
       });
-      console.log("Hello");
       dispatch(setSocket(socket));
-      socket.on('getOnlineUsers',(onlineUsers)=>{
-        dispatch(setOnlineUsers(onlineUsers))
+      socket.on('getOnlineUsers', (onlineUsers) => {
+        dispatch(setOnlineUsers(onlineUsers));
       });
-      return ()=>socket.close();
-    }else{
-      if(socket){
+      return () => socket.close();
+    } else {
+      if (socket) {
         socket.close();
         dispatch(setSocket(null));
       }
     }
-
-  },[authUser]);
+  }, [authUser]);
 
   return (
     <div className="p-4 h-screen flex items-center justify-center">
-    <RouterProvider router={router}/>
+      <RouterProvider router={router} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
